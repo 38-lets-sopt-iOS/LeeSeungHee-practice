9722b4ebe6d5b71f06a9e6ffb2ac382e87d321a7
seunghee <daalgwabyul@naver.com>
2026-05-07 01:45:56 +0900

    회원목록조회 api 실습

---
+++ SOPT38-seminar/week4/GetUserList/GetUsersListViewController.swift
@@ -0,0 +1,73 @@
+//
+//  GetUsersListViewController.swift
+//  SOPT38-seminar
+//
+//  Created by 초긍정행운의포춘쿠키 on 5/2/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class GetUsersListViewController: UIViewController {
+    private var userList: [UserData] = []
+    
+    private let usersListTextView = UITextView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setStyle()
+        setLayout()
+        getUsersList()
+    }
+    
+    private func setUI() {
+        view.addSubview(usersListTextView)
+    }
+    
+    private func setStyle() {
+        view.backgroundColor = .white
+        
+        usersListTextView.do {
+            $0.font = .systemFont(ofSize: 15)
+            $0.isEditable = false
+        }
+    }
+    
+    private func setLayout() {
+        usersListTextView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(20)
+        }
+    }
+    
+    private func getUsersList() {
+        Task{
+            do{
+                let response = try await GetUsersListService.shared.getUserList()
+                userList = response
+                print("유저리스트")
+                updateUI(usersList: userList)
+            } catch {
+                let alert = UIAlertController(
+                    title: "유저 조회 실패",
+                    message: error.localizedDescription,
+                    preferredStyle: .alert
+                )
+                let okAction = UIAlertAction(title: "확인", style: .default)
+                alert.addAction(okAction)
+                self.present(alert, animated: true)
+                
+                print("유저 조회 실패", error)
+            }
+        }
+    }
+    
+    private func updateUI(usersList: [UserData]) {
+        usersListTextView.text = userList
+            .map { "id: \($0.id), 이름: \($0.name), 파트: \($0.part)"}
+            .joined(separator: "\n")
+    }
+    
+}//end
---
+++ SOPT38-seminar/week4/LoginAPIViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAPIViewController.swift
+//  SOPT38-seminar
+//
+//  Created by 초긍정행운의포춘쿠키 on 5/2/26.
+//
+
+import UIKit
+
+class LoginAPIViewController: UIViewController {
+    override func viewDidLoad() {
+    
+        super.viewDidLoad()
+        
+        
+    }
+}
