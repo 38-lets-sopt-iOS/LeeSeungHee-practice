b083417d7ac3ac1b43824db495294d3c1927a305
seunghee <daalgwabyul@naver.com>
2026-05-07 05:05:17 +0900

    feat: 로그인 DTO 작성

---
+++ SOPT38-seminar/week4/Signin/SigninResponseDTO.swift
@@ -0,0 +1,22 @@
+//
+//  SigninResponseDTO.swift
+//  SOPT38-seminar
+//
+//  Created by 초긍정행운의포춘쿠키 on 5/7/26.
+//
+
+import Foundation
+
+struct SigninResponseDTO: Decodable {
+    let success: Bool
+    let status: String
+    let message: String
+    let code: String
+    let data: userId
+    let meta: ErrorResponseDTO?
+    // SignupResponseDTO 에 선언된 구조체와 같음
+}
+
+struct userId: Decodable {
+    let userId: Int
+}
