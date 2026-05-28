5f86c40d5ccbffb15849a9be001a28cb09aef9b3
seunghee <daalgwabyul@naver.com>
2026-05-29 04:22:18 +0900

    feat: 네비게이션 실습

---
+++ SOPT-38-SwiftUI/SOPT-38-SwiftUI/week7/NaviView.swift
@@ -0,0 +1,95 @@
+//
+//  NaviGation.swift
+//  SOPT-38-SwiftUI
+//
+//  Created by 초긍정행운의포춘쿠키 on 5/23/26.
+//
+import SwiftUI
+
+struct Ganadi: Identifiable, Hashable {
+    let id = UUID()
+    let name: String
+    let emoji: String
+    let detail: String
+    
+    static let friends: [Ganadi] = [
+        Ganadi(
+            name: "가나디",
+            emoji: "🐶",
+            detail: "개"
+        ),
+        Ganadi(
+            name: "고냐니",
+            emoji: "🐈",
+            detail: "대장, 본인 이외엔 다 부하, 강하다"
+        ),
+        Ganadi(
+            name: "두도지",
+            emoji: "🧸",
+            detail: "두더지 외계인, 가나디와 친구들을 관찰중, 말할 수 있음"
+        ),
+        Ganadi(
+            name: "공룡이",
+            emoji: "🦕",
+            detail: "바쁜 현대인, 사람친구가 없다, 최근 템플스테이에 관심을 가지고 있다"
+        ),
+        Ganadi(
+            name: "햄스터",
+            emoji: "🐹",
+            detail: "용감하다"
+        ),
+        Ganadi(
+            name: "와와",
+            emoji: "🐕",
+            detail: "성격:미침, 치와와, 수줍음이 많다"
+        )
+    ]
+}
+struct NaviView : View {
+    @State private var isPresented = false
+    var body: some View {
+        NavigationStack {
+            List(Ganadi.friends) {friend in
+                NavigationLink(friend.name, value:friend)
+            }
+            NavigationLink{
+                GanadiImageView()
+            } label: {
+                Text("간단버전")
+            }
+            
+            Spacer(minLength: 100)
+            
+            Button("이동하기 state 버전") {
+                isPresented = true
+            }
+            .navigationDestination(isPresented: $isPresented) {
+                GanadiImageView()
+            }
+            .navigationDestination(for: Ganadi.self) { friend in DetailView(friend: friend)
+            }
+        }
+    }
+}
+
+struct DetailView: View{
+    
+    let friend: Ganadi
+    
+    var body: some View {
+        Text(friend.detail)
+            .navigationTitle(friend.name)
+    }
+}
+
+struct GanadiImageView: View {
+    var body: some View {
+        Image(.c12Ypkypng)
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+#Preview{
+    NaviView()
+}
